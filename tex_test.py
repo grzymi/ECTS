cf568b3f3f910c59776ab1328979359860321748
Przemo Grzymislawski <przemyslaw.grzymislawski@gmail.com>
2019-12-17 12:13:31 +0100

    Still working. Idzie ku dobremu

--- tex_test.py
+++ tex_test.py
@@ -4,8 +4,8 @@ from pylatex.utils import bold, NoEscape
 
 
 def generate_header(przedmiot='Podaj nazwę przedmiotu'):
-    geometry_options = {"margin": "25mm", 'head':'10mm'}
-    doc = Document(geometry_options=geometry_options, lmodern=False)
+    geometry_options = {"margin": "25mm", 'head':'10mm', 'includeheadfoot':False}
+    doc = Document(geometry_options=geometry_options, lmodern=False, page_numbers=True)
     doc.packages.append(Package('roboto', 'default'))
     doc.add_color('PPblue', 'HTML', '006991')
     #doc.packages.append(Package('roboto', 'sfdefault'))
@@ -23,60 +23,121 @@ def generate_header(przedmiot='Podaj nazwę przedmiotu'):
 
     # Add Heading
     with doc.create(FlushLeft()):#align='l')):
-        doc.append(TextColor('white', HugeText(bold(" ")))) #Nie wiem, jak przesunąć tekst w dół
-        doc.append(LineBreak())
-        doc.append(LineBreak())
-        doc.append(LineBreak())
+        doc.append(VerticalSpace('20pt'))
+        #doc.append(TextColor('white', LargeText(bold(" ")))) #Nie wiem, jak przesunąć tekst w dół
+        #doc.append('\n')
         doc.append(LargeText("KARTA OPISU MODUŁU KSZTAŁCENIA"))
         doc.append(LineBreak())
         doc.append(LineBreak())
         doc.append(MediumText("Nazwa modułu/przedmiotu"))
         doc.append(LineBreak())
         doc.append(LargeText(przedmiot))
-        #doc.add_hline(color='blue')
         doc.append(NewLine())
         doc.append(TextColor('PPblue', NoEscape(r"\noindent\rule{\textwidth}{1pt}")))
         doc.append(NewLine())
-        doc.append(MediumText(TextColor('PPblue', 'Przedmiot')))
+        doc.append(MediumText(TextColor('PPblue', bold('Przedmiot'))))
 
     # Dodanie opisu studiów
-    with doc.create(MiniPage(width=r'0.5\textwidth')):
+    with doc.create(MiniPage(width=r'0.6\textwidth')):
         doc.append(MediumText(TextColor('gray', 'Kierunek studiów')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('Energetyka Cieplna i Przemysłowa'))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Forma studiów')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('Stacjonarne/Niestacjonarne'))	#wybierane z listy rozwijanej lub ustawiona stała wartość dla wersji studiów
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Profil studiów')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('Ogólnoakademicki/Praktyczny'))	#wybierane z listy rozwijanej lub ustawiona stała wartość dla wersji studiów
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Język studiów')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('polski'))
-        doc.append(LineBreak())
-        doc.append(VerticalSpace('20pt'))
-#        doc.append(LineBreak())
+        #doc.append(LineBreak())
+        #doc.append(VerticalSpace('20pt'))
 
-    with doc.create(MiniPage(width=r'0.5\textwidth')):
+    with doc.create(MiniPage(width=r'0.6\textwidth')):
         doc.append(MediumText(TextColor('gray', 'Studia w zakresie')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('Chyba trzeba podać specjalność'))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Poziom studiów')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('I lub II stopień'))	#wybierane z listy rozwijanej lub ustawiona stała wartość dla wersji studiów
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Rok/semestr')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('Podać rok i semesrt, z siatki'))	#wybierane z listy rozwijanej lub ustawiona stała wartość dla wersji studiów
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText(TextColor('gray', 'Wymagalność')))
         doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
         doc.append(MediumText('obligatoryjny/obieralny'))
         doc.append('\n')
+
+    with doc.create(FlushLeft()):
+        doc.append(TextColor('PPblue', NoEscape(r"\noindent\rule{\textwidth}{1pt}")))
+        doc.append('\n')
+        doc.append(VerticalSpace('15pt'))
+        doc.append(MediumText(TextColor('PPblue', bold('Liczba godzin'))))
+        #doc.append(VerticalSpace('5pt'))
+
+    with doc.create(MiniPage(width=r'0.6\textwidth')):
+        doc.append(MediumText(TextColor('gray', 'Wykład')))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki'))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText(TextColor('gray', 'Ćwiczenia')))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki'))
+        doc.append('\n')
+
+    with doc.create(MiniPage(width=r'0.6\textwidth')):
+        doc.append(MediumText(TextColor('gray', 'Laboratoria')))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki'))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText(TextColor('gray', 'Projekty/seminaria')))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki'))
+        doc.append('\n')
+
+    with doc.create(FlushLeft()):
+        #doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText(TextColor('gray', 'Inne (np. online)')))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki, trzeba dodać dodatkową opcję'))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText(TextColor('PPblue', bold('Liczba punktów'))))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(MediumText('Wartość z siatki'))
+        doc.append('\n')
+        doc.append(VerticalSpace('5pt'))
+        doc.append(TextColor('PPblue', NoEscape(r"\noindent\rule{\textwidth}{1pt}")))
+
 
 
     doc.generate_pdf("karta", clean_tex=False, compiler='pdflatex')
